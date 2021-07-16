ed44f6579de1f8c4c21162349b3da9e827881597
ngoxuantien <55541378+ngoxuantien@users.noreply.github.com>
2021-07-16 10:22:12 +0700

    Initial Commit

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by xuantien on 16/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
